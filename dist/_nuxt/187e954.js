289b7b0fb6423a8c5c0a2e37869c68d25d6a5c3e
aayushsinghm16 <aayushsinghm16@gmail.com>
2021-06-13 22:49:22 +0530

    dist folder

---
+++ dist/_nuxt/187e954.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{573:function(n,t,e){"use strict";e.r(t);var r=e(46),component=Object(r.a)({},(function(){var n=this,t=n.$createElement;n._self._c;return n._m(0)}),[function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",[e("button",[n._v("\n        Logout\n    ")])])}],!1,null,null,null);t.default=component.exports}}]);
